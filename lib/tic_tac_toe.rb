94f53728442d585093aeda048301a6674b38e3ee
Igli Cekani <htnacekan@gmail.com>
2018-08-19 07:43:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,7 @@ def valid_move?(board, index)
 
   if position_taken?(board, index)==false
     return true
-  elsif board[index].nil?==true
+  elsif board[index].nil?== true
     return false
   end
 
